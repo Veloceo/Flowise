7a7585cc241842128a39a1a1cb0a7fd98c826625
ashmitveloceo <ashmit@veloceo.com>
2024-10-22 15:53:14 +0800

    new changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 # #!/bin/sh
+echo "Hello from the entrypoint script!"
 # ssh -o StrictHostKeyChecking=no -p 7940 -N -g -L 0.0.0.0:1433:172.16.10.2:1433 veloceo@veloceo.endpoints.ezyid.io -i /usr/src/app/vpn/ezy_id_rsa.pem &
 pnpm start
